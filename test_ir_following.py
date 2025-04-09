077827b4dd47fe4bdfcf21b9828e416b9bc2b7cc
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2025-04-09 18:08:43 -0400

    Refactor control loop to use non-async methods and improve sensor reading efficiency

--- test_ir_following.py
+++ test_ir_following.py
@@ -7,6 +7,7 @@ from irobot_create_msgs.action import DriveDistance, RotateAngle
 from geometry_msgs.msg import Twist
 import RPi.GPIO as GPIO
 import time
+import asyncio
 
 class IRLineFollower(Node):
     def __init__(self):
@@ -24,13 +25,12 @@ class IRLineFollower(Node):
         GPIO.setup(self.RIGHT_IR_PIN, GPIO.IN)
         
         # Configuration
-        self.POLL_RATE = 0.001  # seconds between readings
-        self.DRIVE_SPEED = 0.1   # m/s
-        self.ROTATE_SPEED = 0.5  # rad/s
-        self.DRIVE_INCREMENT = 0.05  # meters
-        self.BASE_SPEED = 0.15        # Base forward speed (m/s)
-        self.TURN_FACTOR = 0.5        # Factor to adjust turn intensity
-        self.MIN_ROTATION = 0.1       # Minimum rotation speed (rad/s)
+        self.POLL_RATE = 0.1  # Increased from 0.001 to prevent excessive CPU usage
+        self.DRIVE_SPEED = 0.1
+        self.ROTATE_SPEED = 0.5
+        self.BASE_SPEED = 0.15
+        self.TURN_FACTOR = 0.5
+        self.MIN_ROTATION = 0.1
         
         # Action clients for movement
         self.drive_client = ActionClient(self, DriveDistance, '/drive_distance')
@@ -43,10 +43,10 @@ class IRLineFollower(Node):
             10
         )
         
-        # Modify timer to use async control loop
+        # Create timer without async wrapper
         self.timer = self.create_timer(
-            self.POLL_RATE, 
-            lambda: rclpy.create_task(self.control_loop())
+            self.POLL_RATE,
+            self.timer_callback
         )
         self.get_logger().info('IR Line Follower initialized')
 
@@ -60,31 +60,8 @@ class IRLineFollower(Node):
         self.get_logger().debug(f'IR Values - Left: {left_value}, Right: {right_value}')
         return left_value, right_value
 
-    async def drive_distance(self, distance):
-        goal_msg = DriveDistance.Goal()
-        goal_msg.distance = float(distance)
-        goal_msg.max_translation_speed = self.DRIVE_SPEED
-        
-        self.get_logger().info(f'Driving {distance}m')
-        await self.drive_client.send_goal_async(goal_msg)
-
-    async def rotate_angle(self, angle):
-        goal_msg = RotateAngle.Goal()
-        goal_msg.angle = float(angle)
-        goal_msg.max_rotation_speed = self.ROTATE_SPEED
-        
-        self.get_logger().info(f'Rotating {angle} radians')
-        await self.rotate_client.send_goal_async(goal_msg)
-
-    async def move_robot(self, linear_x, angular_z):
-        """Send velocity commands to the robot"""
-        twist_msg = Twist()
-        twist_msg.linear.x = float(linear_x)
-        twist_msg.angular.z = float(angular_z)
-        self.cmd_vel_pub.publish(twist_msg)
-
-    async def control_loop(self):
-        """Improved control loop with proportional line following"""
+    def timer_callback(self):
+        """Non-async timer callback that handles the control loop"""
         # Read sensor values
         left_sensor, right_sensor = self.read_ir_sensors()
         
@@ -95,7 +72,7 @@ class IRLineFollower(Node):
         # Both sensors off the line (LOW)
         if left_sensor == GPIO.LOW and right_sensor == GPIO.LOW:
             self.get_logger().warn('Both sensors off line!')
-            await self.move_robot(0.0, 0.0)  # Stop
+            self.move_robot(0.0, 0.0)  # Stop
             return
             
         # Both sensors on the line (HIGH) - move forward
@@ -120,7 +97,30 @@ class IRLineFollower(Node):
             angular_speed = self.MIN_ROTATION * (1 if angular_speed > 0 else -1)
 
         # Send movement commands
-        await self.move_robot(linear_speed, angular_speed)
+        self.move_robot(linear_speed, angular_speed)
+
+    def move_robot(self, linear_x, angular_z):
+        """Non-async version of move_robot"""
+        twist_msg = Twist()
+        twist_msg.linear.x = float(linear_x)
+        twist_msg.angular.z = float(angular_z)
+        self.cmd_vel_pub.publish(twist_msg)
+
+    async def drive_distance(self, distance):
+        goal_msg = DriveDistance.Goal()
+        goal_msg.distance = float(distance)
+        goal_msg.max_translation_speed = self.DRIVE_SPEED
+        
+        self.get_logger().info(f'Driving {distance}m')
+        await self.drive_client.send_goal_async(goal_msg)
+
+    async def rotate_angle(self, angle):
+        goal_msg = RotateAngle.Goal()
+        goal_msg.angle = float(angle)
+        goal_msg.max_rotation_speed = self.ROTATE_SPEED
+        
+        self.get_logger().info(f'Rotating {angle} radians')
+        await self.rotate_client.send_goal_async(goal_msg)
 
     def stop_moving(self):
         return self.drive_distance(0.0)
